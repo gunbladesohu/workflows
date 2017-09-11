5292ddda6284b5ef1a2a726f2b44ea670d7fb8e1
gunbladesohu <gunbladesohu@gmail.com>
2017-09-12 10:50:51 +1200

    add style.css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 var concat = require('gulp-concat');
 
 
@@ -12,6 +13,8 @@ var jsSource=[
 'components/scripts/tagline.js',
 'components/scripts/template.js'
 ]
+
+var sassSource = ['components/sass/style.scss'];
 
 
 // gulp.task('log', function(){
@@ -24,8 +27,21 @@ gulp.task('coffee', function(){
 	.pipe(gulp.dest('components/scripts'))
 	;
 });
+
 gulp.task('js',function(){
 	gulp.src(jsSource).pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSource)
+	.pipe(compass({
+		style: 'expanded',
+		sass:'components/sass',
+		image:'builds/development/images'
+	})
+		.on('error',gutil.log))
+	.pipe(gulp.dest('builds/development/css'))
+	;
 });
