28885525560d68fb9e13ea8fcc0e80f83bce95fd
gunbladesohu <gunbladesohu@gmail.com>
2017-09-12 10:01:22 +1200

    added concatenated function and script.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+
+
+var jsSource=[
+'components/scripts/pixgrid.js',
+'components/scripts/rclick.js',
+'components/scripts/tagline.js',
+'components/scripts/template.js'
+]
+
+
 // gulp.task('log', function(){
 // 	gutil.log("workflow is awesome!");
 // });
@@ -10,4 +21,8 @@ gulp.task('coffee', function(){
 		.on('error',gutil.log))
 	.pipe(gulp.dest('components/scripts'))
 	;
+});
+gulp.task('js',function(){
+	gulp.src(jsSource).pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
