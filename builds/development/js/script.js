e7706b3faf18a605349b9e4cefd98cf7edeb4144
gunbladesohu <gunbladesohu@gmail.com>
2017-09-17 19:30:51 +1200

    use it for bootstrap application

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -76,7 +76,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art');
+})(' most creative minds in Art');
 
 fill;
 
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ var coffee = require('gulp-coffee');
 var browserify = require('gulp-browserify');
 var compass = require('gulp-compass');
 var concat = require('gulp-concat');
+var connect = require('gulp-connect');
 
-
+var coffeeSource =['components/coffee/tagline.coffee'];
 
 var jsSource=[
 'components/scripts/pixgrid.js',
@@ -15,13 +16,31 @@ var jsSource=[
 ]
 
 var sassSource = ['components/sass/style.scss'];
+var sassSource_all = [
+'components/sass/style.scss',
+'components/sass/_base.scss',
+'components/sass/_minxins.scss',
+'components/sass/_modules.scss',
+'components/sass/_pixgrid.scss',
+'components/sass/_rclick.scss',
+'components/sass/_variables.scss',
+];
 
 
 // gulp.task('log', function(){
 // 	gutil.log("workflow is awesome!");
 // });
+gulp.task('connect', function()
+	{
+		connect.server(
+		{
+			root: 'builds/development',
+			livereload: true
+		});
+	});
+
 gulp.task('coffee', function(){
-	gulp.src('components/coffee/tagline.coffee')
+	gulp.src(coffeeSource)
 	.pipe(coffee({bare:true})
 		.on('error',gutil.log))
 	.pipe(gulp.dest('components/scripts'))
@@ -32,6 +51,7 @@ gulp.task('js',function(){
 	gulp.src(jsSource).pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
+	.pipe(connect.reload());
 });
 
 gulp.task('compass', function(){
@@ -43,7 +63,14 @@ gulp.task('compass', function(){
 	})
 		.on('error',gutil.log))
 	.pipe(gulp.dest('builds/development/css'))
-	;
+	.pipe(connect.reload());
 });
 
-gulp.task('default', ['coffee','js','compass']);
+gulp.task('watch',function(){
+	gulp.watch(coffeeSource, ['coffee']);
+	gulp.watch(jsSource, ['js']);
+	gulp.watch(sassSource_all, ['compass']);
+})
+
+
+gulp.task('default', ['coffee','js','compass','connect','watch']);
